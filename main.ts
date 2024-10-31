4d39afb1c823a0801a8fd12ec3e290c1cde38343
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2024-10-31 18:23:56 -0500

    Add condig deno

---
+++ main.ts
@@ -0,0 +1,13 @@
+// import { main } from "./reto_1/solution.ts";
+import { main } from "./reto_2/solution.ts";
+
+
+function retoOne() {
+  main()
+}
+
+function retoTwo() {
+  main()
+}
+
+retoTwo()
